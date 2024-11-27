912be1b4ee0f01e5b2fb5d12fed08b8c573e519d
acidlord <acidlord_34@abv.bg>
2024-11-28 00:31:11 +0200

    Add Order Entity

--- AnimalStudio.Data.Models/AnimalProcedure.cs
+++ AnimalStudio.Data.Models/AnimalProcedure.cs
@@ -1,19 +1,23 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using static AnimalStudio.Common.EntityValidationConstants.Animal;
 
 namespace AnimalStudio.Data.Models
 {
-    public class AnimalProcedure
-    {
-        public int AnimalId { get; set; }
-        [ForeignKey(nameof(AnimalId))]
-        public virtual Animal Animal { get; set; } = null!;
+	public class AnimalProcedure
+	{
+		public int AnimalId { get; set; }
+		[ForeignKey(nameof(AnimalId))]
+		public virtual Animal Animal { get; set; } = null!;
 
-        public int ProcedureId { get; set; }
-        [ForeignKey(nameof(ProcedureId))]
-        public virtual Procedure Procedure { get; set; } = null!;
+		public int ProcedureId { get; set; }
+		[ForeignKey(nameof(ProcedureId))]
+		public virtual Procedure Procedure { get; set; } = null!;
 
-        [Required]
-        public string UserId { get; set; } = null!;
-    }
+		[Required]
+		[MaxLength(OwnerIdMaxLength)]
+		public string UserId { get; set; } = null!;
+
+		public Order Order { get; set; } = null!;
+	}
 }
---
+++ AnimalStudio.Data.Models/Order.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static AnimalStudio.Common.EntityValidationConstants.Animal;
+
+namespace AnimalStudio.Data.Models
+{
+	[Comment("Order entity")]
+	public class Order
+	{
+		[Key]
+		public Guid Id { get; set; }
+
+		[Required]
+		[Comment("The user who is owner of the animal")]
+		[MaxLength(OwnerIdMaxLength)]
+		public string UserId { get; set; } = null!;
+
+		[Required]
+		[ForeignKey(nameof(UserId))]
+		[Comment("The owner of the order")]
+		public IdentityUser User { get; set; } = null!;
+
+		public ICollection<AnimalProcedure> AnimalsProcedures { get; set; } = new List<AnimalProcedure>();
+	}
+}
