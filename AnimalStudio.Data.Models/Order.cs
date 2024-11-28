cb9927a9803cd27734474364efbc4d5a40c3f97e
Angel <acidlord_34@abv.bg>
2024-11-28 18:18:39 +0200

    Added check for authenticated user in layout for My Zone nav-item.

--- AnimalStudio.Data.Models/Order.cs
+++ AnimalStudio.Data.Models/Order.cs
@@ -13,7 +13,7 @@ namespace AnimalStudio.Data.Models
 		public Guid Id { get; set; }
 
 		[Required]
-		[Comment("The user who is owner of the animal")]
+		[Comment("Identifier of the owner of the order")]
 		[MaxLength(OwnerIdMaxLength)]
 		public string OwnerId { get; set; } = null!;
 
@@ -22,11 +22,21 @@ namespace AnimalStudio.Data.Models
 		[Comment("The owner of the order")]
 		public IdentityUser Owner { get; set; } = null!;
 
+		[Required]
+		[Comment("Identifier of the animal in the order")]
 		public int AnimalId { get; set; }
+
+		[Required]
+		[Comment("The animal in order")]
 		[ForeignKey(nameof(AnimalId))]
 		public virtual Animal Animal { get; set; } = null!;
 
+		[Required]
+		[Comment("Identifier of the procedure in the order")]
 		public int ProcedureId { get; set; }
+
+		[Required]
+		[Comment("The procedure in order")]
 		[ForeignKey(nameof(ProcedureId))]
 		public virtual Procedure Procedure { get; set; } = null!;
 
